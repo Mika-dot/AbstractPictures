cf549b307d4baea8e9d7a53050807a3d28b78ad6
Serebrovsky Sergey Maksimovich <61110689+Mika-dot@users.noreply.github.com>
2024-02-14 22:43:20 +0300

    Update Program.cs

--- SavingPhotosFromSite/Program.cs
+++ SavingPhotosFromSite/Program.cs
@@ -10,7 +10,7 @@ WebClient client = new WebClient();
 
 for (int i = 1; i <= 40; i++)
 {
-    string imageUrl = $"https://www.girlstop.info/cat/posts/659aacc79101c9.37620893/{i}.webp";
+    string imageUrl = $"https://www.пфффф.info/{i}.webp";
     string filePath = $"{folderPath}\\photo_{i}.jpg";
 
     client.DownloadFile(imageUrl, filePath);
